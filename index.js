0e798b1c50cb4dc415aea3fd11d587c8a33643b6
candacehernandez <chernandez.aggie@gmail.com>
2022-02-04 19:12:48 -0600

    commit tree 886b5b3d727a28c9fd2f0615752f54e919b20906

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 // Write your solution here!
+
 const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+function prependCat(name){
+    const newArray = [name, ...cats];
+    return newArray;
+}
+
+function removeLastCat(){
+    return cats.slice(0,cats.length - 1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
+/*const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name){
     return cats.push(name);
@@ -33,4 +70,4 @@ function removeLastCat(name){
 
 function removeFirstCat(name){
     return cats.slice(1);
-}
+}*/
